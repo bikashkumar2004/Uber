4a1eff4ba0604200f0448d01bbda0dcfc6f26e74
Bikash Kumar Choudhury <kumarbikashchoudhury2004@gmail.com>
2024-12-22 18:59:54 +0530

    user login done

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,13 +1,31 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import { body } from 'express-validator';
-import { registerUser } from '../controllers/user.controller.js';
+import { body } from "express-validator";
+import { registerUser, loginUser } from "../controllers/user.controller.js";
 
-router.post('/register', [
-    body('email').isEmail().withMessage('invalid email'),
-    body('password').isLength({min: 6}).withMessage('Password must be at least 6 characters long'),
-    body('fullname.firstname').isLength({ min: 3 }).withMessage('First name must be at least 3 characters long')
-], registerUser);
+router.post(
+  "/register",
+  [
+    body("email").isEmail().withMessage("invalid email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+    body("fullname.firstname")
+      .isLength({ min: 3 })
+      .withMessage("First name must be at least 3 characters long"),
+  ],
+  registerUser
+);
 
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("invalid email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  loginUser
+);
 
-export default router
+export default router;
